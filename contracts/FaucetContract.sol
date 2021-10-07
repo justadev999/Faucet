f3c3f67dff7db66468feaf1de4c296ba36139abf
RainboWZRD <bucciolsimone@gmail.com>
2021-10-07 20:48:39 +0200

    index of addresses

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,11 +3,25 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract FaucetContract {
     
-    address[] public funders;
+    address[] internal funders;
 
-    function addFunds() external payable {
+    function addFunds() public payable {
         funders.push(msg.sender);
     }
 
+    function getAllFunders() internal view returns (address[] memory) {
+        return funders;
+    }
+
+    function getFunderAtIndex(uint8 index) internal view returns(address) {
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
+    }
+
+
 receive() external payable{}
 }
+
+
+// let instance = await FaucetContract.deployed();
+// instance.addFunds({from:accounts[0], value:"2"})
