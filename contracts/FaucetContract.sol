32d6e6db8ecdfa96c58af645167389372cbabaf0
RainboWZRD <bucciolsimone@gmail.com>
2021-10-07 22:13:55 +0200

    owenrship, withdraw and modifiers

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,25 +3,71 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract FaucetContract {
     
-    address[] internal funders;
+    uint public numOfFAunders;
+    mapping (address => bool) private funders;
+    mapping (uint => address) private lutFunders;
+    address public owner;
 
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier OnlyOwner{
+        require(msg.sender == owner, "Nope, you're not ETH TO'S CEO!!!!!"); _;
+    }
+
+    //Ready for mr.Tambussi
+    function transferThePower(address newOwner) OnlyOwner {
+        owner = newOwner;
+    }
+
+    modifier LimitWithdraw(uint  withdrawAmount){
+         require
+         (withdrawAmount < 1000000000000000000,
+          "Nope, too much bro");
+          _;
+    }
+
+    //DEPOSIT
     function addFunds() public payable {
-        funders.push(msg.sender);
+        address funder = msg.sender;
+
+        if(!funders[funder]){
+            uint index = numOfFAunders++;
+            funders[funder] = true;
+            lutFunders[index] = funder;
+        }
     }
 
-    function getAllFunders() internal view returns (address[] memory) {
-        return funders;
+    //WITHDRAW
+    function withdraw(uint withdrawAmount) external LimitWithdraw(withdrawAmount) {
+         payable(msg.sender).transfer(withdrawAmount);
     }
 
-    function getFunderAtIndex(uint8 index) internal view returns(address) {
-        address[] memory _funders = getAllFunders();
-        return _funders[index];
+    // function getAllFunders() internal view returns (address[] memory) {
+    //     return funders;
+    // }
+    function getAllFunders () external view returns(address[] memory) {
+        address[] memory _funders = new address[](numOfFAunders);
+
+        for(uint i = 0; i < numOfFAunders; i++){
+            _funders[i] = lutFunders[i];
+        }
+        return _funders;
     }
 
+    function getFunderAtIndex(uint8 index) external view returns(address) {
+        return lutFunders[index];
+    }
 
 receive() external payable{}
 }
 
 
 // let instance = await FaucetContract.deployed();
-// instance.addFunds({from:accounts[0], value:"2"})
+// instance.addFunds({from:accounts[0], value:"1000000000000000000"})
+// instance.addFunds({from:accounts[1], value:"1000000000000000000"})
+// instance.addFunds({from:accounts[2], value:"1000000000000000000"})
+// instance.getFunderAtIndex(1);
+// instance.withdraw("5000000000000000000", {from: accounts[1]})
+
